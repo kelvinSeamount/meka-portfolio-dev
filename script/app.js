e04fae7f8fd92dde3e2c0a83d04b9cec7ac0e09a
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-07-03 01:04:51 +0200

    fix mobile nav-bar

--- script/app.js
+++ script/app.js
@@ -1,5 +1,33 @@
 const scrollNav = document.querySelector("nav");
 
+const typeEl = document.querySelector(".type");
+
 document.addEventListener("scroll", function () {
   scrollNav.classList.toggle("window-scroll", window.scrollY > 0);
 });
+
+const words = ["Front-End", "Developer", "Based", "In", "Bremen", "Germany"];
+
+let count = 0;
+
+let letterCount = 0;
+
+// Create a function
+const displayType = function () {
+  if (count === words.length) {
+    count = 0;
+  }
+
+  //select a current word to be displayed
+  let displayWords = words[count];
+
+  //to display the words letter by letter
+  let letter = displayWords.slice(0, ++letterCount);
+  typeEl.textContent = letter;
+  if (letter.length === displayWords.length) {
+    letterCount = 0;
+    count++;
+  }
+  setTimeout(displayType, 380);
+};
+displayType();
